db03c1f470868a815e6a59e20750c19dce48bee0
elvirtroche <elvirtroche@gmail.com>
2026-01-20 14:46:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function agendarCita() {
-    const telefono = "595983553347"; // Reemplaza con tu número real
+    const telefono = "595983736827"; // Reemplaza con tu número real
     const mensaje = encodeURIComponent("¡Hola! Me gustaría agendar una cita para manicura.");
     window.open(`https://wa.me/${telefono}?text=${mensaje}`, '_blank');
 }
